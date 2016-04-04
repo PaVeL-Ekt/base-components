bafbe0fbea5f8360c8609c8de1b51f815bd17672
Chernoskutov <pavel-ekt@yandex.ru>
2016-04-04 15:38:26 +0500

    fix WrongParameterType exception
    set parameter previous is default to null

--- src/exceptions/WrongParameterTypeException.php
+++ src/exceptions/WrongParameterTypeException.php
@@ -6,7 +6,7 @@ use PavelEkt\BaseComponents\Abstracts\BaseException;
 
 class WrongParameterTypeException extends BaseException
 {
-    public function __construct($paramName, $expectedType, $givenType, $previous)
+    public function __construct($paramName, $expectedType, $givenType, $previous = null)
     {
         parent::__construct(
             'Type of parameter \'' . $paramName . '\' expected \'' . $expectedType . '\' but \'' . $givenType . '\' given.',
